240b0461020ce233f75ec5da74a82aa30f2ed7c4
ErykEG <erykegonzalez@gmail.com>
2023-03-16 17:56:00 -0600

    Organization

---
+++ tc2005b_403_evidence/src/Components/App.js
@@ -0,0 +1,12 @@
+import "./Styles/App.css";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="danger">Danger</Button>{" "}
+    </div>
+  );
+}
+
+export default App;
