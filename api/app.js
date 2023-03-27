3f1d1115d9e413c11865878595ad242d872d7d5d
ErykEG <erykegonzalez@gmail.com>
2023-03-27 17:22:22 -0600

    Test

--- api/app.js
+++ api/app.js
@@ -13,8 +13,7 @@ app.get("/candidates", async (request, result) => {
   await sql.connect(
     "Server=tcp:miservidorsqlequipo2.database.windows.net,1433;Initial Catalog=TC2005B_403_Equipo2;Persist Security Info=False;User ID=usuarioazure;Password=TC2005B403e2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   );
-  const resultQuerie =
-    await sql.query`select Id_Candidates as Id, Name_Candidates as Nombre from Candidates`;
+  const resultQuerie = await sql.query``;
   result.json(resultQuerie.recordset);
 });
 app.listen(port, function () {
