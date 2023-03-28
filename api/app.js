f46537d47829b9576e391b6134e7f8fa79ebaf15
ErykEG <erykegonzalez@gmail.com>
2023-03-27 18:49:17 -0600

    Remove insert

--- api/app.js
+++ api/app.js
@@ -15,7 +15,6 @@ app.get("/azure", async (request, result) => {
     "Server=tcp:miservidorsqlequipo2.database.windows.net,1433;Initial Catalog=TC2005B_403_Equipo2;Persist Security Info=False;User ID=usuarioazure;Password=TC2005B403e2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   );
   // Querie request -> Querie proyection
-  sql.query`INSERT INTO Candidates (Id_Candidates, Name_Candidates) Values (50, 'Jorge');`;
   const resultQuerie = await sql.query`select * from candidates`; // Comentario
   result.json(resultQuerie.recordset);
 });
