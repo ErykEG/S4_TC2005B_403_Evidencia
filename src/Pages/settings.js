b866ad2b1ec63b43a68de513932b0db9be6f15a1
ErykEG <erykegonzalez@gmail.com>
2023-04-12 02:26:20 -0600

    Agregar Styles

---
+++ src/Pages/settings.js
@@ -0,0 +1,13 @@
+import "../Components/Styles/Settings.css";
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      <p>Configuracion de la cuenta</p>
+      <p>Preferencias del usuario</p>
+      <p>Importar Bases de Datos</p>
+    </div>
+  );
+};
+
+export default Settings;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Auth0ProviderWithNavigate from "./Components/Auth0_Provider_With_Navigate";
 import { BrowserRouter } from "react-router-dom";
-import NavBar from "./Components/Nav_Bar";
 import { useAuth0 } from "@auth0/auth0-react";
+import NavBar from "./Components/Nav_Bar";
+import "rsuite/dist/rsuite.min.css";
+import { CustomProvider } from "rsuite";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
