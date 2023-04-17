841672b20a7055f4ea8ac1ae19a5930bcc2138e1
ErykEG <erykegonzalez@gmail.com>
2023-04-17 15:52:55 -0600

    Button Settings

--- src/Pages/settings.js
+++ src/Pages/settings.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useState, useEffect } from "react";
-import "../Components/Styles/settings.css";
+import "../Components/Styles/Settings.css";
 
 function Settings() {
   const { user } = useAuth0();
@@ -11,29 +11,29 @@ function Settings() {
   useEffect(() => {
     const userRoles = user?.[`${process.env.REACT_APP_AUTH0_NAMESPACE}`] ?? [];
     console.log(`URL: ${process.env.REACT_APP_AUTH0_NAMESPACE}`);
-    setIsAdmin(userRoles.includes("Admin"));
+    setIsAdmin(userRoles.includes("Super-Manager"));
   }, [user]);
 
   return (
     <div className="settings-panel">
       {isAdmin ? (
-        <div className="admin-settings" style={{height: "100vh"}}>
+        <div className="admin-settings" style={{ height: "100vh" }}>
           <h3>Settings</h3>
           <p>Configuracion de la cuenta</p>
           <p>Preferencias del usuario</p>
-          <p>Importar bases de datos</p>
           <h3 id="adminset">Admin Settings</h3>
           <p>Mis proyectos</p>
           <p>Registro de candidatos</p>
           <p>Registro de proyectos</p>
           <p>Condiguración de reglas y filtros</p>
-      </div>
+          <p>Importar bases de datos</p>
+          <button>Export Database to Excel</button>
+        </div>
       ) : (
-        <div className="normal-settings" style={{height: "100vh"}}>
+        <div className="normal-settings" style={{ height: "100vh" }}>
           <h3>Settings</h3>
           <p>Configuracion de la cuenta</p>
           <p>Preferencias del usuario</p>
-          <p>Importar bases de datos</p>
         </div>
       )}
     </div>
