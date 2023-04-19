bb4f8fe17513cea515e6cfaca474283a246e762d
ErykEG <erykegonzalez@gmail.com>
2023-04-18 21:00:43 -0600

    Fixed assigned

--- src/Pages/profile_page.js
+++ src/Pages/profile_page.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import RedirectLogin from "../Components/Redirect_Login";
 import Logout from "../Components/Logout";
 import "../Components/Styles/profile_page.css";
 
@@ -12,17 +11,6 @@ function Profile() {
     return <div> Loading... </div>;
   }
   console.log(user);
-
-  if (user && !user.email_verified) {
-    return (
-      <div>
-        <RedirectLogin
-          mensaje="Please confirm your email before login. Redirecting to main page in 5
-      seconds..."
-        />
-      </div>
-    );
-  }
 
   const userRoles = user?.[`${process.env.REACT_APP_AUTH0_NAMESPACE}`] ?? [];
 
